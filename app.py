5d767ca9fb18aecb32075d10fe63ffc864f290d3
Matthew Rease <self@matthewrease.net>
2025-01-05 19:52:51 -0800

    Initial cache concept + pagination.
    Don't to cache everything cause at some point you're just replacing SQL
    with Python RAM...
    
    Also pages now work, even with filters.

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 """Compressed Thoughts blog, by Matthew Rease."""
 
 import threading
-from datetime import datetime
+from datetime import datetime, timedelta
 from os import environ
 
 import markdown
 import MySQLdb
 from dotenv import load_dotenv
 from flask import Flask, render_template
+
+TWO_HOUR_DELTA = timedelta(hours = 2)
 
 class HeadingShiftProcessor(markdown.treeprocessors.Treeprocessor):  # pylint: disable=too-few-public-methods
     """Find all <h> elements and increase their level."""
@@ -41,15 +43,24 @@ class DBContextManager:
         self.cursor = self.db.cursor()
         self.lock = threading.Lock()
 
-    #def execute(self, *args):
-    #    """Dumb wrapper for MySQLdb.cursor.execute."""
-    #    with self.lock:
-    #        self.cursor.execute(args)
-    #        return self.cursor.fetchall()
+        _2ha = datetime.now() - TWO_HOUR_DELTA
+        self.cache = {
+            'all_posts': {},
+            'all_posts_req': _2ha,
+
+            'top_tags': [],
+            'top_tags_req': _2ha,
+
+            'users': {},
+
+            'post_count': 0,
+            'post_count_req': _2ha
+        }
 
     def execute(self, query, tokens):
         """Dumb wrapper for MySQLdb.cursor.execute."""
         with self.lock:
+            print(f'\x1b[31mexecuting query {query}\x1b[0m')
             self.cursor.execute(query, tokens)
             return self.cursor.fetchall()
 
@@ -62,26 +73,70 @@ class DBContextManager:
         self.cursor.close()
         self.db.close()
 
-def generate_archive_dict(posts):
-    """Split flat dictionary of posts into one sorted by year and then month."""
-    res = {}
-    for year in range(datetime.now().year, 2019, -1):
-        year = str(year)
-        y_posts = {}
-        for month in range(1, 13):
-            month = str(month).zfill(2)
-            m_posts = {
-                _id[6:]: post
-                for _id, post in posts.items()
-                if _id[0:4] == year and _id[4:6] == month }
-            #for _id, post in posts.items():
-            #    if _id[0:4] == year and _id[4:6] == month:
-            #        y_posts[month][_id[6:]] = post
-            if m_posts:
-                y_posts[month] = m_posts
-        if y_posts:
-            res[year] = y_posts
-    return res
+    def get_all_posts_sidebar(self):
+        """Get short-form dict of every post (with cache)."""
+        now = datetime.now()
+        if now - self.cache['all_posts_req'] > TWO_HOUR_DELTA:
+            self.cache['all_posts_req'] = now
+            self.cache['all_posts'] = {
+                row[2]: { 'title': row[0], 'description': row[1], 'alt': row[3] }
+                for row in self.execute(
+                    'SELECT title, description, filename, image FROM Post ORDER BY filename DESC',
+                    tuple())}
+        return self.cache['all_posts']
+
+    def generate_archive_dict(self):
+        """Split flat dictionary of posts into one sorted by year and then month."""
+        posts = self.get_all_posts_sidebar()
+        res = {}
+        for year in range(datetime.now().year, 2019, -1):
+            year = str(year)
+            y_posts = {}
+            for month in range(1, 13):
+                month = str(month).zfill(2)
+                m_posts = {
+                    _id[6:]: post
+                    for _id, post in posts.items()
+                    if _id[0:4] == year and _id[4:6] == month }
+                #for _id, post in posts.items():
+                #    if _id[0:4] == year and _id[4:6] == month:
+                #        y_posts[month][_id[6:]] = post
+                if m_posts:
+                    y_posts[month] = m_posts
+            if y_posts:
+                res[year] = y_posts
+        return res
+
+    def get_top_tags(self):
+        """Get most common post tags (with cache)."""
+        now = datetime.now()
+        if now - self.cache['top_tags_req'] > TWO_HOUR_DELTA:
+            self.cache['top_tags_req'] = now
+            self.cache['top_tags'] = [row[0] for row in context.execute("""
+                SELECT Tag.name, COUNT(DISTINCT PostTag.post_id) AS post_count
+                FROM PostTag
+                JOIN Tag on PostTag.tag_id = Tag.tag_id
+                GROUP BY Tag.name
+                ORDER BY post_count DESC
+                LIMIT 15""",
+                tuple())]
+        return self.cache['top_tags']
+
+    def get_user(self, user_id):
+        """Retrieve the name of the user with given ID."""
+        if user_id not in self.cache['users']:
+            self.cache['users'][user_id] = self.execute(
+                'SELECT name FROM User WHERE user_id = %s;',
+                (user_id,))[0][0]
+        return self.cache['users'][user_id]
+
+    def get_post_count(self):
+        """Get total number of blog posts."""
+        now = datetime.now()
+        if now - self.cache['post_count_req'] > TWO_HOUR_DELTA:
+            self.cache['post_count_req'] = now
+            self.cache['post_count'] = self.execute('SELECT COUNT(*) FROM Post;', tuple())[0][0]
+        return self.cache['post_count']
 
 # For Flask
 app = Flask(__name__)
@@ -90,33 +145,16 @@ context = DBContextManager()
 # Handle Pages
 @app.route('/')
 @app.route('/filter/<tag_filter>')
-def index(tag_filter=None):
+@app.route('/page/<int:page>')
+@app.route('/filter/<tag_filter>/page/<int:page>')
+def index(tag_filter='', page=0):
     """Generate main page, showing most recent posts (paginated)."""
-    if not tag_filter:
-        tag_filter = ''
-    top_tags = [row[0] for row in context.execute("""
-        SELECT Tag.name, COUNT(DISTINCT PostTag.post_id) AS post_count
-        FROM PostTag
-        JOIN Tag on PostTag.tag_id = Tag.tag_id
-        GROUP BY Tag.name
-        ORDER BY post_count DESC
-        LIMIT 15""",
-        tuple())]
+    top_tags = [tag for tag in context.get_top_tags()]
     if tag_filter in top_tags:
         top_tags.remove(tag_filter)
+    if tag_filter != '':
         top_tags.insert(0, tag_filter)
-
-    query = f"""
-        SELECT User.name, Post.title, Post.description, Post.preview, Post.published, Post.modified, Post.filename, Post.image
-        FROM Post
-        JOIN User ON Post.user_id = User.user_id {'''
-        WHERE Post.post_id IN (
-            SELECT PostTag.post_id
-            FROM PostTag
-            JOIN Tag ON Tag.tag_id = PostTag.tag_id
-            WHERE Tag.name = %s)''' if tag_filter != '' else ''}
-        ORDER BY filename DESC
-        LIMIT 5;"""
+    top_tags.insert(0, '')
 
     main_posts = [
         {
@@ -137,13 +175,22 @@ def index(tag_filter=None):
             },
             'datestr': row[4].strftime('%b %d, %Y')
         }
-        for row in context.execute(query, (tag_filter,) if tag_filter != '' else None)]
-
-    all_posts = {
-        row[2]: { 'title': row[0], 'description': row[1], 'alt': row[3] }
         for row in context.execute(
-            'SELECT title, description, filename, image FROM Post ORDER BY filename DESC',
-            tuple())}
+            f"""
+            SELECT User.name, Post.title, Post.description, Post.preview, Post.published, Post.modified, Post.filename, Post.image
+            FROM Post
+            JOIN User ON Post.user_id = User.user_id {'''
+            WHERE Post.post_id IN (
+                SELECT PostTag.post_id
+                FROM PostTag
+                JOIN Tag ON Tag.tag_id = PostTag.tag_id
+                WHERE LOWER(Tag.name) LIKE %s)''' if tag_filter != '' else ''}
+            ORDER BY filename DESC
+            LIMIT 5
+            OFFSET %s;""",
+            (f'%{tag_filter.lower()}%', page * 5) if tag_filter != '' else (page * 5,))]
+
+    all_posts = context.get_all_posts_sidebar()
 
     popular_posts = [ '202002101957', '202002261145', '202004161413' ]
     metadata = {
@@ -158,9 +205,9 @@ def index(tag_filter=None):
                 'description': all_posts[id]['description']
             }
             for id in popular_posts],
-        'tags': [''] + top_tags,
+        'tags': top_tags,
         'filter': tag_filter,
-        'archive': generate_archive_dict(all_posts),
+        'archive': context.generate_archive_dict(),
         'month_names': {
             '01': 'January',
             '02': 'February',
@@ -175,11 +222,18 @@ def index(tag_filter=None):
             '11': 'November',
             '12': 'December'
         },
+        'page': page,
+        'last': (context.get_post_count() - 1) // 5 if tag_filter == '' else (context.execute(
+            """
+            SELECT COUNT(DISTINCT PostTag.post_id)
+            FROM PostTag
+            JOIN Tag ON Tag.tag_id = PostTag.tag_id
+            WHERE LOWER(Tag.name) LIKE %s;""",
+            (f'%{tag_filter.lower()}%',))[0][0] - 1) // 5,
         'posts': main_posts
     }
-    print(metadata['archive'])
+    print(metadata['last'])
     return render_template('index.html', metadata=metadata)
-    #return '<html><body>Hello World</body></html>'
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -193,6 +247,24 @@ def show_post(post_id):
         (post_id,))[0]
     md_body = markdown.markdown(res[5], extensions=["fenced_code", HeadingShiftExtension()])
 
+    # Alternate idea to get both tags_sql and the post in one query:
+    #
+    # SELECT Post.post_id, Post.title, ..., Tag.name
+    # FROM Post
+    # LEFT JOIN PostTag ON Post.post_id = PostTag.post_id
+    # LEFT JOIN Tag ON PostTag.tag_id = Tag.tag_id
+    # WHERE Post.filename = %s;
+    #
+    # Which creates something like the table below.
+    #
+    # +---------+------------------------------------+--------------+
+    # | post_id | title                              | name         |
+    # +---------+------------------------------------+--------------+
+    # |      19 | Confidentiality in the Digital Age | learning     |
+    # |      19 | Confidentiality in the Digital Age | cryptography |
+    # |      19 | Confidentiality in the Digital Age | privacy      |
+    # +---------+------------------------------------+--------------+
+
     tags_sql = context.execute(f"""
         SELECT Tag.name
         FROM PostTag
@@ -200,36 +272,40 @@ def show_post(post_id):
         WHERE PostTag.post_id = {res[0]};""",  # Not explicitly optimized
         tuple())                               # Should do the WHERE first...
 
-    author = context.execute(f'SELECT name FROM User WHERE user_id = {res[1]};', tuple())[0][0]
+    author = context.get_user(res[1])
 
+    all_posts = context.get_all_posts_sidebar()
+
+    popular_posts = [ '202002101957', '202002261145', '202004161413' ]
     metadata = {
         'base': '',
         'canonical': f'post/{res[8]}',
         'popular': [
             {
-                'id': 202002101957,
-                'image': 'https://cdn.matthewrease.net/blog/202002101957.webp',
-                'image_alt': 'html code on a green crt display',
-                'title': 'Old Code',
-                'description': 'Ever look at your old code, and think: what the actual ****?'
-            },
-            {
-                'id': 202002261145,
-                'image': 'https://cdn.matthewrease.net/blog/202002261145.webp',
-                'image_alt': 'a cluttered attic with cardboard boxes, among other things',
-                'title': 'The Attic of Unfinished Projects',
-                'description': "It makes me kinda sad to think about all the projects I've started that are \"on the backburner\", \"unfinished\", or otherwise in some form of purgatory."
-            },
-            {
-                'id': 202004161413,
-                'image': 'https://cdn.matthewrease.net/blog/202004161413.webp',
-                'image_alt': "coronavirus image that definitely wasn't just the first image search result :)",
-                'title': 'Coronavirus am I right?',
-                'description': 'Ha, what kind of idiot would start a blog, then go over a month without posting... heh... yeah what kind?'
+                'id': id,
+                'image': f'https://cdn.matthewrease.net/blog/{id}.webp',
+                'image_alt': all_posts[id]['alt'],
+                'title': all_posts[id]['title'],
+                'description': all_posts[id]['description']
             }
-        ],
+            for id in popular_posts],
         'tags': [''] + [row[0] for row in tags_sql],
         'filter': '',
+        'archive': context.generate_archive_dict(),
+        'month_names': {
+            '01': 'January',
+            '02': 'February',
+            '03': 'March',
+            '04': 'April',
+            '05': 'May',
+            '06': 'June',
+            '07': 'July',
+            '08': 'August',
+            '09': 'September',
+            '10': 'October',
+            '11': 'November',
+            '12': 'December'
+        },
         'title': res[2],
         'description': res[3],
         'author': author,
